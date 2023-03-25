1a3315f54a29c5054c0f980040bcf6d78f564f2f
shinjaewon99 <tlswodnjs99@naver.com>
2023-03-26 00:14:42 +0900

    feat : 포맷터 구현 및 테스트 작성

---
+++ typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+@Slf4j
+// Number : Integer, Float, Long 등 추상클래스를 가지고있다.
+public class MyNumberFormatter implements Formatter<Number> {
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        // "1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
+
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        NumberFormat instance = NumberFormat.getInstance(locale);
+        return instance.format(object);
+    }
+}
